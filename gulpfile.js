05146107397e38c1ac292194df21098a6ef7ea7c
donald-s <don4thirteen@gmail.com>
2014-12-16 14:24:04 -0500

    Adding flavor to index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var ftp = require('gulp-ftp');
+var connect = require('gulp-connect');
 
 var paths = {
-    sass: ['./css/scss/*.scss'],
-    css: './css/compiled'
+    sass: ['./css/scss/**/*.scss'],
+    css: './css/compiled',
+    html: ['./*.html']
 };
 
-gulp.task('sass', function() {
-    gulp.src(paths.sass)
-        .pipe(sass())
-        .pipe(gulp.dest(paths.css));
-});
-
-gulp.task('ftp', function() {
+gulp.task('ftp', function () {
     gulp.src(paths.css + "/*.css")
         .pipe(ftp({
             host: 'ftp.wellencpa.com',
@@ -22,7 +18,33 @@ gulp.task('ftp', function() {
         }));
 });
 
-gulp.task('watch', function() {
-   gulp.watch(paths.sass, ['sass']);
-//   gulp.watch(paths.css + "/*.css", ['ftp']);
+gulp.task('connect', function () {
+    connect.server({
+        root: './',
+        livereload: true
+    });
 });
+
+gulp.task('sass', function () {
+    gulp.src(paths.sass)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('css', function() {
+    gulp.src(paths.css)
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+    gulp.src('./*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.css, ['css']);
+    gulp.watch(paths.html, ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
